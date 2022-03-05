304356d40e6e47d1f01b8e335a08f53cadd31b03
Mohit <mohitgadhavi1@gmail.com>
2022-03-05 22:08:33 +0530

    reg

---
+++ main.py
@@ -0,0 +1,90 @@
+# capitalize()	Converts the first character to upper case
+print("Hello, World".capitalize())
+# casefold()	Converts string into lower case
+print("Hello, World".capitalize())
+# center()	Returns a centered string
+print("Hello, World".capitalize())
+# count()	Returns the number of times a specified value occurs in a string
+print("Hello, World".capitalize())
+# encode()	Returns an encoded version of the string
+print("Hello, World".capitalize())
+# endswith()	Returns true if the string ends with the specified value
+print("Hello, World".capitalize())
+# expandtabs()	Sets the tab size of the string
+print("Hello, World".capitalize())
+# find()	Searches the string for a specified value and returns the position of where it was found
+print("Hello, World".capitalize())
+# format()	Formats specified values in a string
+print("Hello, World".capitalize())
+# format_map()	Formats specified values in a string
+print("Hello, World".capitalize())
+# index()	Searches the string for a specified value and returns the position of where it was found
+print("Hello, World".capitalize())
+# isalnum()	Returns True if all characters in the string are alphanumeric
+print("Hello, World".capitalize())
+# isalpha()	Returns True if all characters in the string are in the alphabet
+print("Hello, World".capitalize())
+# isdecimal()	Returns True if all characters in the string are decimals
+print("Hello, World".capitalize())
+# isdigit()	Returns True if all characters in the string are digits
+print("Hello, World".capitalize())
+# isidentifier()	Returns True if the string is an identifier
+print("Hello, World".capitalize())
+# islower()	Returns True if all characters in the string are lower case
+print("Hello, World".capitalize())
+# isnumeric()	Returns True if all characters in the string are numeric
+print("Hello, World".capitalize())
+# isprintable()	Returns True if all characters in the string are printable
+print("Hello, World".capitalize())
+# isspace()	Returns True if all characters in the string are whitespaces
+print("Hello, World".capitalize())
+# istitle()	Returns True if the string follows the rules of a title
+print("Hello, World".capitalize())
+# isupper()	Returns True if all characters in the string are upper case
+print("Hello, World".capitalize())
+# join()	Joins the elements of an iterable to the end of the string
+print("Hello, World".capitalize())
+# ljust()	Returns a left justified version of the string
+print("Hello, World".capitalize())
+# lower()	Converts a string into lower case
+print("Hello, World".capitalize())
+# lstrip()	Returns a left trim version of the string
+print("Hello, World".capitalize())
+# maketrans()	Returns a translation table to be used in translations
+print("Hello, World".capitalize())
+# partition()	Returns a tuple where the string is parted into three parts
+print("Hello, World".capitalize())
+# replace()	Returns a string where a specified value is replaced with a specified value
+print("Hello, World".capitalize())
+# rfind()	Searches the string for a specified value and returns the last position of where it was found
+print("Hello, World".capitalize())
+# rindex()	Searches the string for a specified value and returns the last position of where it was found
+print("Hello, World".capitalize())
+# rjust()	Returns a right justified version of the string
+print("Hello, World".capitalize())
+# rpartition()	Returns a tuple where the string is parted into three parts
+print("Hello, World".capitalize())
+# rsplit()	Splits the string at the specified separator, and returns a list
+print("Hello, World".capitalize())
+# rstrip()	Returns a right trim version of the string
+print("Hello, World".capitalize())
+# split()	Splits the string at the specified separator, and returns a list
+print("Hello, World".capitalize())
+# splitlines()	Splits the string at line breaks and returns a list
+print("Hello, World".capitalize())
+# startswith()	Returns true if the string starts with the specified value
+print("Hello, World".capitalize())
+# strip()	Returns a trimmed version of the string
+print("Hello, World".capitalize())
+# swapcase()	Swaps cases, lower case becomes upper case and vice versa
+print("Hello, World".capitalize())
+# title()	Converts the first character of each word to upper case
+print("Hello, World".capitalize())
+# translate()	Returns a translated string
+print("Hello, World".capitalize())
+# upper()	Converts a string into upper case
+print("Hello, World".capitalize())
+# zfill()	Fills the string with a specified number of 0 values at the beginning
+print("Hello, World".capitalize())
+name = input('Tell me your name:')
+print('hi '+ name)

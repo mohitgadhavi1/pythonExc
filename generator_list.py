332cf3218d63c95d126040befedc97236b406923
Mohit Gadhavi <mohit.gadhavi@zybisys.com>
2023-03-24 18:48:42 +0530

    new file added

---
+++ generator_list.py
@@ -0,0 +1,7 @@
+'''
+In Python, a generator is a type of iterator that
+generates values on-the-fly as you iterate over it,
+whereas a list is a collection of values that are 
+stored in memory all at once. Here are some key 
+differences between generators and lists:
+'''
---
+++ popular_imports.py
@@ -0,0 +1,29 @@
+'''
+os : - provides a way to interact with the operating system, allowing you to work with files and directories, 
+and run other system commands.
+
+sys : allows you to interact with the interpreter itself, giving you access to command-line arguments, 
+error messages, and other low-level details.
+
+math : provides access to many mathematical functions, such as trigonometric functions, logarithms, and more.
+
+datetime :  provides classes for working with dates and times, including support for time zones,
+ formatting and parsing dates, and calculating time intervals.
+
+random :  provides functions for generating random numbers, random selections from lists, and more.
+
+re :  provides support for regular expressions, allowing you to match and manipulate text patterns.
+
+json : provides support for encoding and decoding JSON data, which is a popular data interchange format used in web applications.
+
+requests :  provides a simple way to make HTTP requests and work with web APIs.
+
+numpy :  provides a powerful array processing library, allowing you to work with large arrays of numerical data efficiently.
+
+pandas : provides high-performance data structures and data analysis tools, allowing you to work with tabular data
+ (similar to a spreadsheet) in Python.
+
+matplotlib : provides a library for creating data visualizations, including line plots, scatter plots, histograms, and more.
+
+scikit-learn : provides a library of machine learning algorithms and tools, allowing you to build and train predictive models in Python.
+'''

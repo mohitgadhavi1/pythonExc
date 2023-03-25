6bb76c9adcfb324b02c69392ccd9834ebc8500c6
Mohit Gadhavi <mohit.gadhavi@zybisys.com>
2023-03-25 13:13:57 +0530

    new file added

---
+++ package.py
@@ -0,0 +1,54 @@
+'''
+Choose a name for your package. 
+It's recommended to use all lowercase letters
+ and underscores for spaces. For example, 
+ if you want to create a package for handling 
+ dates, you could name it "date_utils".
+
+Create a new directory with the name of your
+ package. In this example, you would create
+   a directory called "date_utils".
+
+Inside the directory, create a file 
+called __init__.py. This file can be empty or
+ contain some code that should be executed
+ when the package is imported.
+
+Create the modules for your package. 
+These are the Python files that contain the 
+actual code for your package. For example,
+ you could create a module called 
+ "date_parser.py" that contains a 
+ function for parsing dates.
+
+Optionally, you can include a setup.py 
+file in the root of your package directory. 
+This file contains information about your 
+package, such as its name, version, author, 
+and dependencies.
+
+Optionally, you can also include a README.md 
+file in the root of your package directory. 
+This file should contain information about 
+your package, such as its purpose, usage, 
+and installation instructions.
+Here's an example directory structure for 
+the "date_utils" package:
+'''
+
+#date_utils/
+#    __init__.py
+#    date_parser.py
+#    setup.py
+#    README.md
+
+
+'''
+Once you've created your package, 
+you can use it in other Python code by 
+importing it like any other module. 
+For example, to use the parse_date function 
+from the date_parser module in the date_utils 
+package, you would write:
+'''
+#from date_utils.date_parser import parse_date
